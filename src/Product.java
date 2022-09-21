6af5eb376a273eed3acf8647c10d24eb954a9460
Selim <selim-kose@live.com>
2022-09-21 10:07:07 +0200

    liten uppdatering

--- src/Product.java
+++ src/Product.java
@@ -1,8 +1,53 @@
 public class Product {
 
-    private String name;
-    private String catagory;
     private int id;
-    private int ordPrice;
+    private String name;
+    private String brand;
+    private String category;
+    private double price;
 
+
+    public Product(){
+
+    }
+
+
+    public Product(int id, String name, String brand, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.category = category;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
